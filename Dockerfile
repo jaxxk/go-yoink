5470fb294ec7c43f89575a0b92eb2fbaa13c91ba
jaxxk <forwarwick@live.com>
2024-06-05 10:13:43 -0700

    sql configs

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,4 @@ FROM postgres:16.3-alpine3.20
 
 # docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -d -p 5432:5432 postgres:16.3-alpine3.20
 # docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' some-postgres
+# psql "postgres://postgres:@localhost:5432/blogator"
--- main.go
+++ main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/jaxxk/go-yoink/internal/database"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
+	}
+
+	CONN := os.Getenv("CONN")
+	db, err := sql.Open("postgres", CONN)
+
+	config := apiConfig{
+		DB: database.New(db),
 	}
 
 	PORT := os.Getenv("PORT")
