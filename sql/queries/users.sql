0d0acf2c8ef74d68e0b405d2621b436a31b37f78
Jaxxk <forwarwick@live.com>
2024-06-11 16:27:56 -0700

    add sql for get user by apikey

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -2,3 +2,8 @@
 INSERT INTO users (id, created_at, updated_at, name)
 VALUES ($1, $2, $3, $4)
 RETURNING *;
+
+-- name: GetUserByApiKey :one
+SELECT id, created_at, updated_at, name, api_key
+FROM users
+WHERE api_key = $1;
