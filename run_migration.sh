5ea591bf327c83a929cc677170c6a45031631954
Jaxxk <forwarwick@live.com>
2024-06-11 15:04:27 -0700

    working add user

--- run_migration.sh
+++ run_migration.sh
@@ -2,7 +2,7 @@
 
 # Load environment variables from .env file
 if [ -f .env ]; then
-  export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
+  export $(grep -v '^#' .env | xargs)
 else
   echo ".env file not found!"
   exit 1
