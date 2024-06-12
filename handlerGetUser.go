12f1aa37ea473560570e3daaf582e687825489a9
jaxxk <forwarwick@live.com>
2024-06-12 16:42:26 -0700

    part of middleware changes

--- handlerGetUser.go
+++ handlerGetUser.go
@@ -1,33 +1,11 @@
 package main
 
 import (
-	"context"
-	"log"
 	"net/http"
-	"strings"
-	"time"
+
+	"github.com/jaxxk/go-yoink/internal/database"
 )
 
-func (cfg *apiConfig) handlerGetUserByApiKey(w http.ResponseWriter, r *http.Request) {
-	apiKeyString := r.Header.Get("authorization")
-	if apiKeyString == "" {
-		respondWithError(w, http.StatusBadRequest, "missing api key")
-	}
-	apiKey := parseApiKey(apiKeyString)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel() // ensure the cancel function is called to release resources
-
-	dbUser, err := cfg.DB.GetUserByApiKey(ctx, apiKey)
-	if err != nil {
-		log.Printf("API key is: %s", apiKey)
-		respondWithError(w, http.StatusBadRequest, "failed to retireve user from api key")
-	}
-
-	respondWithJSON(w, http.StatusOK, dbUser)
-}
-
-func parseApiKey(s string) string {
-	strList := strings.Split(s, " ")
-	return strList[1]
+func (cfg *apiConfig) HandlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
+	respondWithJSON(w, http.StatusOK, user)
 }
