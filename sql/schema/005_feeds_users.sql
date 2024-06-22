32189933306e3d8ee50c1f4713e6d047f7bced16
jaxxk <forwarwick@live.com>
2024-06-22 10:00:56 -0700

    working

---
+++ sql/schema/005_feeds_users.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- Step 1: Create feeds_users table
+CREATE TABLE feeds_users (
+    id VARCHAR(128) NOT NULL PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    user_id VARCHAR(128),
+    feed_id VARCHAR(128),
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    CONSTRAINT fk_feed FOREIGN KEY (feed_id) REFERENCES feeds(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+-- Step 1: Drop the feeds_users table
+DROP TABLE feeds_users;
